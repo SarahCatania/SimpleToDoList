06555eee06815548aeda3c2c77cbe7a013c18544
SarahCatania <sarahcatania9@gmail.com>
2021-03-03 21:47:54 -0800

    Prework

--- app/src/main/java/com/example/simpletodo/ItemsAdapter.java
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -12,10 +12,19 @@ import java.util.List;
 
 //Responsible for displaying data from the model into a row in the recycler view
 public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
+
+    public interface OnLongClickListener
+    {
+        void onItemLongClicked(int position);
+    }
+
     List<String> items;
-    public ItemsAdapter(List<String> items)
+    OnLongClickListener longClickListener;
+
+    public ItemsAdapter(List<String> items, OnLongClickListener longClickListener)
     {
         this.items = items;
+        this.longClickListener = longClickListener;
     }
 
     @NonNull
@@ -57,6 +66,15 @@ public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
         public void bind(String item)
         {
             tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v)
+                {
+                    // Notify the listener which position was long pressed
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+            });
         }
     }
 }
